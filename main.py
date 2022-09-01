15bfb0a2e5848e7120bf02da9f7aec038406a2cc
Pavy93 <pavel_yanochkin@mail.ru>
2022-09-01 14:45:51 +0300

    main.py with click

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-Main module
+Main module to work with PostgreSQL through CLI.
 """
 import click
 import json
@@ -9,22 +9,45 @@ from utils.sql_queries_collection import query_insert_data_to_rooms_table, query
     result_queries_list
 
 
+@click.command()
+@click.option("--rooms-file", "-rf", default=None, help="Path to json file to insert data in rooms table.")
+@click.option("--students-file", "-sf", default=None, help="Path to json file to insert data in students table.")
+@click.option("--out-folder", "-outf", default='D:/docker/python_project/data',
+              help="Folder where the results will be saved to.")
+@click.option("--out-files-extension", "-ofe", default=None, help="Extension of files where results of the queries will"
+                                                                  " be saved. Only JSON and XML.")
 def work_with_postresql(rooms_file: str, students_file: str, out_folder: str, out_files_extension: str):
-    __config_path = 'config/db_config.json'
+    """
+    function "work_with_postresql" help to work with PostgreSQL through CLI
+    :param rooms_file: Path to json file to insert data in rooms table.
+    :type:rooms_file: str
+    :param students_file: Path to json file to insert data in students table.
+    :type:students_file: str
+    :param out_folder: Folder where the results will be saved to.
+    :type:out_folder: str
+    :param out_files_extension: Extension of files where results of the queries will be saved. Only JSON and XML.
+    :type:out_files_extension: str
+    """
+    __config_path: str = 'config/db_config.json'
     db_config: dict = FileManager().read_json_file(__config_path)
 
     with ConnectAndCursorManager(**db_config) as cur:
-        data_rooms = FileManager().read_json_file(rooms_file)
-        data_students = FileManager().read_json_file(students_file)
-        cur.execute(query_insert_data_to_rooms_table, (json.dumps(data_rooms),))
-        cur.execute(query_insert_data_to_students_table, (json.dumps(data_students),))
+        if rooms_file and students_file:
+            data_rooms = FileManager().read_json_file(rooms_file)
+            data_students = FileManager().read_json_file(students_file)
+            cur.execute(query_insert_data_to_rooms_table, (json.dumps(data_rooms),))
+            cur.execute(query_insert_data_to_students_table, (json.dumps(data_students),))
 
-        out_files_names = ['students_by_rooms_num', 'top_5_lowest_avg_age', 'top_5_highest_age_diff',
-                           'room_diversity_list']
+        out_files_names: list = ['students_by_rooms_num', 'top_5_lowest_avg_age', 'top_5_highest_age_diff',
+                                 'room_diversity_list']
 
         if out_files_extension:
             for query, file_name in zip(result_queries_list, out_files_names):
                 cur.execute(query)
-                result = cur.fetchall()
-                path_to_save = f'{out_folder}/{file_name}.{out_files_extension}'
+                result: list = cur.fetchall()
+                path_to_save: str = f'{out_folder}/{file_name}.{out_files_extension}'
                 FileManager().save_to_file(result[0][0], path_to_save)
+
+
+if __name__ == "__main__":
+    work_with_postresql()
--- utils/sql_queries_collection.py
+++ utils/sql_queries_collection.py
@@ -1,19 +1,19 @@
 """
 Module contains required SQL queries
 """
-query_insert_data_to_rooms_table = """INSERT INTO rooms
+query_insert_data_to_rooms_table: str = """INSERT INTO rooms
                 SELECT * FROM JSON_POPULATE_RECORDSET(NULL::rooms, %s);"""
 
-query_insert_data_to_students_table = """INSERT INTO students
+query_insert_data_to_students_table: str = """INSERT INTO students
                 SELECT * FROM JSON_POPULATE_RECORDSET(NULL::students, %s);"""
 
-query_students_by_rooms_num = """SELECT * FROM students_num_view;"""
+query_students_by_rooms_num: str = """SELECT * FROM students_num_view;"""
 
-query_top_5_lowest_avg_age = """SELECT * FROM top_5_lowest_avg_age_view;"""
+query_top_5_lowest_avg_age: str = """SELECT * FROM top_5_lowest_avg_age_view;"""
 
-query_top_5_highest_age_diff = """SELECT * FROM top_5_highest_age_diff_view;"""
+query_top_5_highest_age_diff: str = """SELECT * FROM top_5_highest_age_diff_view;"""
 
-query_room_diversity_list = """SELECT * FROM room_diversity_list_view;"""
+query_room_diversity_list: str = """SELECT * FROM room_diversity_list_view;"""
 
-result_queries_list = [query_students_by_rooms_num, query_top_5_lowest_avg_age, query_top_5_highest_age_diff,
-                       query_room_diversity_list]
+result_queries_list: list = [query_students_by_rooms_num, query_top_5_lowest_avg_age, query_top_5_highest_age_diff,
+                             query_room_diversity_list]
